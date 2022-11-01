c1cc32b4514b6edf7e625b07a735148acb4a237a
Alba Cardona <albal347@yahoo.es>
2022-10-31 21:25:30 -0500

    Inicio Sprint 1

---
+++ app/src/main/java/com/example/vinilosgrupo3/models/Album.kt
@@ -0,0 +1,11 @@
+package com.example.vinilosgrupo3.models
+
+data class Album (
+    val albumId:Int,
+    val name:String,
+    val cover:String,
+    val releaseDate:String,
+    val description:String,
+    val genre:String,
+    val recordLabel:String
+)
---
+++ app/src/main/java/com/example/vinilosgrupo3/viewmodels/DetailViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.vinilosgrupo3.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.*
+import com.example.vinilosgrupo3.models.Album
+import com.example.vinilosgrupo3.network.NetworkServiceAdapter
+
+class DetailViewModel(application: Application, albumId: Int) :  AndroidViewModel(application) {
+
+    private val _detail = MutableLiveData<Album>()
+
+    val detail: LiveData<Album>
+        get() = _detail
+
+    private var _eventNetworkError = MutableLiveData<Boolean>(false)
+
+    val eventNetworkError: LiveData<Boolean>
+        get() = _eventNetworkError
+
+    private var _isNetworkErrorShown = MutableLiveData<Boolean>(false)
+
+    val isNetworkErrorShown: LiveData<Boolean>
+        get() = _isNetworkErrorShown
+
+    val id:Int = albumId
+
+    init {
+        refreshDataFromNetwork()
+    }
+
+    private fun refreshDataFromNetwork() {
+        NetworkServiceAdapter.getInstance(getApplication()).getDetail(id, {
+            _detail.postValue(it)
+            _eventNetworkError.value = false
+            _isNetworkErrorShown.value = false
+        },{
+            _eventNetworkError.value = true
+        })
+    }
+
+    fun onNetworkErrorShown() {
+        _isNetworkErrorShown.value = true
+    }
+
+    class Factory(val app: Application, val albumId: Int) : ViewModelProvider.Factory {
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
+                @Suppress("UNCHECKED_CAST")
+                return DetailViewModel(app, albumId) as T
+            }
+            throw IllegalArgumentException("Unable to construct viewmodel")
+        }
+    }
+}
