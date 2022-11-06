fd9d801216b3ef821c52a369470d699a5db3068e
Carlos <c.villamilc@uniandes.edu.co>
2022-11-06 15:26:58 -0500

    Se agrega paquete repository

---
+++ app/src/main/java/com/example/vinilosgrupo3/repositories/AlbumRepository.kt
@@ -0,0 +1,17 @@
+package com.example.vinilosgrupo3.repositories
+
+import android.app.Application
+import com.android.volley.VolleyError
+import com.example.vinilosgrupo3.models.Album
+import com.example.vinilosgrupo3.network.NetworkServiceAdapter
+
+class AlbumRepository (val application: Application) {
+    fun refreshData(callback: (List<Album>)->Unit, onError: (VolleyError)->Unit){
+        NetworkServiceAdapter.getInstance(application).getAlbums({
+            callback(it)
+        },{
+            onError
+        })
+
+    }
+}
