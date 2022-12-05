7d226417ea64edf8de810a55da1119a6a63faa1e
PatrickMykoda <p.mykoda@uniandes.edu.co>
2022-12-04 19:12:31 -0500

    Agregando Test E2E para la creación de albumes

--- app/src/androidTest/java/espresso_e2e/EspressoTest_e2e.java
+++ app/src/androidTest/java/espresso_e2e/EspressoTest_e2e.java
@@ -3,6 +3,7 @@ package espresso_e2e;
 import static android.os.SystemClock.sleep;
 import static  androidx.test.espresso.Espresso.*;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static  androidx.test.espresso.assertion.ViewAssertions.matches;
 import static  androidx.test.espresso.matcher.ViewMatchers.*;
 
@@ -43,7 +44,7 @@ public class EspressoTest_e2e {
 
         sleep(1000);
         //click on button "visitantes"
-        onView(allOf(withId(R.id.button), isDisplayed())).perform(click());
+        onView(allOf(withId(R.id.button_visitor), isDisplayed())).perform(click());
 
         sleep(1000);
         // click on album in list
@@ -145,6 +146,75 @@ public class EspressoTest_e2e {
         // return to album catalog and check if album catalog is displayed
         pressBack();
         onView(allOf(withId(R.id.collectorsRv), isDisplayed()));
+
+    }
+
+    @Test
+    public void crearAlbum() {
+
+        sleep(1000);
+        //click on button "coleccionistas"
+        onView(allOf(withId(R.id.button_collector), isDisplayed())).perform(click());
+
+        sleep(1000);
+        //click on button "crear album"
+        onView(allOf(withId(R.id.button_create_album), isDisplayed())).perform(click());
+
+        sleep(1000);
+        //click on input "record label"
+        onView(allOf(withId(R.id.spinner_record_label), isDisplayed())).perform(click());
+
+        sleep(1000);
+        // click on record label
+        onView(allOf(withText("EMI"), isDisplayed())).perform(click());
+
+        sleep(1000);
+        //click on input "genre"
+        onView(allOf(withId(R.id.spinner_genre), isDisplayed())).perform(click());
+
+        sleep(1000);
+        // click on genre
+        onView(allOf(withText("Rock"), isDisplayed())).perform(click());
+
+        sleep(1000);
+        //click on input "description"
+        onView(allOf(withId(R.id.album_description), isDisplayed())).perform(click());
+
+        sleep(1000);
+        // type description
+        onView(withId(R.id.album_description_id)).perform(typeText("American Idiot es el album mas popular de Green Day"));
+        pressBack();
+
+        sleep(1000);
+        //click on input "release date"
+        onView(allOf(withId(R.id.album_releaseDate), isDisplayed())).perform(click());
+
+        sleep(1000);
+        // type release date
+        onView(withId(R.id.album_releaseDate_id)).perform(typeText("2004-08-01T00:00:00-05:00"));
+        pressBack();
+
+        sleep(1000);
+        //click on input "cover"
+        onView(allOf(withId(R.id.album_cover), isDisplayed())).perform(click());
+
+        sleep(1000);
+        // type cover URL
+        onView(withId(R.id.album_cover_id)).perform(typeText("https://upload.wikimedia.org/wikipedia/en/e/ed/Green_Day_-_American_Idiot_album_cover.png"));
+        pressBack();
+
+        sleep(1000);
+        //click on input "name"
+        onView(allOf(withId(R.id.album_name), isDisplayed())).perform(click());
+
+        sleep(1000);
+        // type album name
+        onView(withId(R.id.album_name_id)).perform(typeText("American Idiot"));
+        pressBack();
+
+        sleep(1000);
+        //click on button "create album"
+        onView(allOf(withId(R.id.button_create_album), isDisplayed())).perform(click());
 
     }
 
